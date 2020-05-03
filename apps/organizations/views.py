2239d8eb93d2717c377bda139b259fa83a8a7673
xiong <email@example.com2760282782@qq.com>
2020-05-03 10:30:28 +0800

    排序功能

--- apps/organizations/views.py
+++ apps/organizations/views.py
@@ -11,16 +11,35 @@ class OrgListView(View):
     def get(self, request, *args, **kwargs):
 
         all_orgs = CourseOrg.objects.all()
-        all_orgs_nums = CourseOrg.objects.count()
         all_citys = City.objects.all()
+
+        # 根据机构查询
+        category = request.GET.get('ct', '')
+        if category:
+            all_orgs = all_orgs.filter(category=category)
+
+        # 根据城市查询
+        city_id = request.GET.get('city', '')
+        if city_id:
+            if city_id.isdigit():  # 判断对象是不是字符串
+                all_orgs = all_orgs.filter(city_id=int(city_id))
+
+        # 根据学生人数、课程数排序
+        sort = request.GET.get('sort', '')
+        if sort == 'students':
+            all_orgs = all_orgs.order_by('-students')
+        elif sort == 'courses':
+            all_orgs = all_orgs.order_by('-course_nums')
+
+        # 机构数量
+        all_orgs_nums = all_orgs.count()
 
         # 设置分页器
         try:
             page = request.GET.get('page', 1)
         except PageNotAnInteger:
             page = 1
-
-        p = Paginator(all_orgs,per_page=3, request=request)
+        p = Paginator(all_orgs, per_page=3, request=request)
         # orgs是page对象，而不是queryset对象，前端调用要有object_list
         orgs = p.page(page)
 
@@ -28,4 +47,7 @@ class OrgListView(View):
             'all_orgs': orgs,
             'all_citys': all_citys,
             'all_orgs_nums': all_orgs_nums,
+            'category': category,
+            'city_id': city_id,
+            'sort': sort,
         })
